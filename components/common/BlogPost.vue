696d78a4a26da611e03df9f717d8dbe8ab017124
Enzo Falcon <falcone134@gmail.com>
2024-10-10 23:29:54 -0500

    Design blog post

--- components/common/BlogPost.vue
+++ components/common/BlogPost.vue
@@ -1,23 +1,66 @@
 <template>
-  <MDC v-if="body.trim()" v-slot="{ data, body }" :value="fullBody">
-    <article class="p-4 prose dark:prose-invert" v-bind="$attrs">
+  <MDC v-if="post.body.trim()" v-slot="{ data, body }" :value="fullBody">
+    <article
+      class="p-4 prose dark:prose-invert"
+      :class="{ 'max-w-[1000px]': props.isArticle, 'h-[700px] overflow-y-scroll': !props.isArticle }"
+      v-bind="$attrs"
+    >
+      <div>
+        <h1>{{ data.title }}</h1>
+        <div>
+          <Typography color="gray" tag="p">
+            By: <span class="text-primary-500">Enzo Falcón</span><br>
+            Last updated on: <span class="text-primary-500">{{
+              data.updatedAt ? new Date(data.updatedAt).toLocaleDateString() : new Date().toLocaleDateString()
+            }}</span>
+          </Typography>
+        </div>
+        <div v-if="data.tags" class="flex gap-3 flex-wrap mt-4">
+          <UiBadge v-for="tag in data.tags.split(',')" :label="tag" />
+        </div>
+        <p>{{ data.description }}</p>
+      </div>
       <MDCRenderer v-if="body" :body="body" :data="data" />
     </article>
   </MDC>
 </template>
 
 <script setup lang="ts">
+import type { CorePost, BlogPost, Post } from "~/types";
+
 const props = defineProps<{
-  title: string;
-  description: string;
-  extraData?: string;
-  body: string;
+  post: CorePost | BlogPost | Post;
+  isArticle?: boolean;
 }>();
 
+function isStringArray(value: unknown): value is string[] {
+  if (!Array.isArray(value)) {
+    return false;
+  }
+  return !value.some((v) => typeof v !== "string");
+}
+
+const stringifyTags = computed(() => {
+
+  const tags = props.post.tags
+  if (tags.length === 0) {
+    return ""
+  } else if (isStringArray(tags)) {
+    return tags.join(",")
+  } else {
+    return tags.map((tag) => tag.name).join(",")
+  }
+})
+
 const header = computed(() => {
-  return `---\ntitle: ${props.title}\ndescription: ${props.description}\n---`;
+  return `---
+title: ${props.post.title}
+description: ${props.post.description}
+tags: ${stringifyTags.value}
+updatedAt: ${"updatedAt" in props.post ? props.post.updatedAt : ""}
+---`;
 });
 const fullBody = computed(() => {
-  return `${header.value}\n${props.body}`;
+  return `${header.value}\n${props.post.body}`;
 });
 </script>
--- components/ui/Badge.vue
+++ components/ui/Badge.vue
@@ -1,9 +1,9 @@
 <template>
   <div
-    class="flex text-white items-center gap-1 w-fit rounded-xl px-2 py-1 shadow-[0_0_0_2px] shadow-primary-600 dark:shadow-primary-400 bg-primary-500 dark:bg-primary-600"
+    class="flex text-white items-center gap-1 w-fit rounded-xl px-2 py-0.5 shadow-[0_0_0_2px] shadow-primary-600 dark:shadow-primary-400 bg-primary-500 dark:bg-primary-600"
   >
-    <div class="flex items-center w-4 h-4">
-      <UIcon :name="props.icon" />
+    <div v-if="props.icon" class="flex items-center w-4 h-4">
+      <UIcon :name="props.icon"/>
     </div>
     <div>
       <span class="text-sm">{{ props.label }}</span>
@@ -13,7 +13,7 @@
 
 <script setup lang="ts">
 const props = defineProps<{
-  icon: string;
+  icon?: string;
   label: string;
 }>();
 </script>
