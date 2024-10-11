c97acfe671c9d17fd65f3e25b226579110067a23
Enzo Falcon <falcone134@gmail.com>
2024-10-11 01:30:24 -0500

    change blog post detail view

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -1,17 +1,34 @@
 <template>
   <div class="w-full">
-    <CommonBlogPost
-      class="mx-auto"
-      :title="post?.title"
-      :description="post?.description"
-      :body="post?.body"
-    />
+    <div class="mb-4 mx-auto max-w-[1000px]">
+      <UButton size="sm" color="gray" icon="i-mdi-chevron-left" variant="link" to="/blog">
+        Go Back
+      </UButton>
+    </div>
+    <CommonBlogPost class="mx-auto" :post="post as BlogPost" is-article />
   </div>
 </template>
 
 <script lang="ts" setup>
+import type { BlogPost } from "~/types";
+
 const route = useRoute();
 const slug = route.params.slug;
 const { fetchPost } = usePosts();
 const post = await fetchPost(slug as string);
+
+if (!post) {
+  throw showError({
+    statusCode: 404,
+    statusMessage: "Post does not exist",
+  })
+}
+
+useSeoMeta({
+  title: post?.title,
+  description: post?.description,
+  ogTitle: post?.title,
+  ogDescription: post?.description
+})
+
 </script>
